5ce11e47d88c5dbe8b4b5931ed8cf5e13507dcc9
MichaelOfCourse <borisperso.mj@gmail.com>
2015-12-03 22:46:31 +0100

    Ajout Site Web
    Site web ajouté

---
+++ NdLSiteWeb/index.php
@@ -0,0 +1,14 @@
+<?
+    include("header.php");
+?>
+<style type="text/css">
+    #map {
+        width: 1920px;
+        height: 980px;
+    }
+</style>
+
+<section>
+  
+  <div id="map"></div>
+</section>
